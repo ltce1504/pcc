65fec54193d93ae640604699b45fe297d2dd80ce
ltceuzr <ur7900045@gmail.com>
2026-04-19 22:03:54 +0530

    Update p7.c

--- p7.c
+++ p7.c
@@ -1,176 +1,101 @@
 #include <stdio.h>
-#include <malloc.h>
-
-struct Node {
+#include <stdlib.h>
+typedef struct Node {
     int data;
     struct Node* next;
-};
+} node;
 
-// Function to create a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+node* createNode(int data)
+{
+    node* newNode = (node*)malloc(sizeof(node));
+
+    if (newNode == NULL)
+        {
+            printf("Overflow");
+            return NULL;
+        }
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
 }
 
-// Function to insert a new element at the beginning
-void insertAtFirst(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
-}
+int insertBeforeHead(node** head, int data)
+{
+    node* newNode = createNode(data);
 
-// Function to insert at the end
-void insertAtEnd(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
+    if (!newNode)
+        return -1;
 
     if (*head == NULL) {
         *head = newNode;
-        return;
+        return 0;
     }
-
-    struct Node* temp = *head;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newNode;
+    newNode->next = *head;
+    *head = newNode;
+    return 0;
 }
 
-// Function to insert at a specific position
-void insertAtPosition(struct Node** head, int data, int position) {
-    struct Node* newNode = createNode(data);
-
-    if (position == 0) {
-        insertAtFirst(head, data);
-        return;
-    }
-
-    struct Node* temp = *head;
-    for (int i = 0; temp != NULL && i < position - 1; i++) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL) {
-        printf("Position out of range\n");
-        free(newNode);
-        return;
-    }
-
-    newNode->next = temp->next;
-    temp->next = newNode;
-}
-
-// Function to delete from beginning
-void deleteFromFirst(struct Node** head) {
-    if (*head == NULL) {
-        printf("List is empty\n");
-        return;
-    }
-
-    struct Node* temp = *head;
-    *head = temp->next;
+int deleteHead(node** head)
+{
+    node* temp = *head;
+    *head = (*head)->next;
     free(temp);
+    return 0;
 }
 
-// Function to delete from end
-void deleteFromEnd(struct Node** head) {
-    if (*head == NULL) {
-        printf("List is empty\n");
-        return;
+int isEmpty(node** stack) { return *stack == NULL; }
+
+void push(node** stack, int data)
+{
+    if (insertBeforeHead(stack, data) == NULL) {
+        printf("Stack Overflow!\n");
     }
-
-    struct Node* temp = *head;
-
-    if (temp->next == NULL) {
-        free(temp);
-        *head = NULL;
-        return;
-    }
-
-    while (temp->next->next != NULL) {
-        temp = temp->next;
-    }
-
-    free(temp->next);
-    temp->next = NULL;
 }
 
-// Function to delete at position
-void deleteAtPosition(struct Node** head, int position) {
-    if (*head == NULL) {
-        printf("List is empty\n");
-        return;
+int pop(node** stack)
+{
+    if (isEmpty(stack)) {
+        printf("Stack Underflow\n");
+        return -1;
     }
 
-    struct Node* temp = *head;
-
-    if (position == 0) {
-        deleteFromFirst(head);
-        return;
-    }
-
-    for (int i = 0; temp != NULL && i < position - 1; i++) {
-        temp = temp->next;
-    }
-
-    if (temp == NULL || temp->next == NULL) {
-        printf("Position out of range\n");
-        return;
-    }
-
-    struct Node* next = temp->next->next;
-    free(temp->next);
-    temp->next = next;
+    deleteHead(stack);
+    return 0;
 }
 
-// Function to print the list
-void print(struct Node* head) {
-    struct Node* temp = head;
+int peek(node** stack)
+{
+    if (!isEmpty(stack))
+        return (*stack)->data;
+    else
+        return -1;
+}
 
+void printStack(node** stack)
+{
+    node* temp = *stack;
     while (temp != NULL) {
-        printf("%d -> ", temp->data);
+        printf("%d-> ", temp->data);
         temp = temp->next;
     }
-
-    printf("NULL\n");
+    printf("\n");
 }
 
-// Driver code
-int main() {
-    struct Node* head = NULL;
+int main()
+{
+    node* stack = NULL;
 
-    insertAtFirst(&head, 10);
-    printf("Linked list after inserting 10 at beginning:\n");
-    print(head);
+    push(&stack, 10);
+    push(&stack, 20);
+    push(&stack, 30);
+    push(&stack, 40);
+    push(&stack, 50);
 
-    printf("Linked list after inserting 20 at end:\n");
-    insertAtEnd(&head, 20);
-    print(head);
-
-    printf("Linked list after inserting 5 at end:\n");
-    insertAtEnd(&head, 5);
-    print(head);
-
-    printf("Linked list after inserting 30 at end:\n");
-    insertAtEnd(&head, 30);
-    print(head);
-
-    printf("Linked list after inserting 15 at position 2:\n");
-    insertAtPosition(&head, 15, 2);
-    print(head);
-
-    printf("Linked list after deleting first node:\n");
-    deleteFromFirst(&head);
-    print(head);
-
-    printf("Linked list after deleting last node:\n");
-    deleteFromEnd(&head);
-    print(head);
-
-    printf("Linked list after deleting node at position 1:\n");
-    deleteAtPosition(&head, 1);
-    print(head);
-
+    printf("Stack: ");
+    printStack(&stack);
+    pop(&stack);
+    pop(&stack);
+    printf("\nStack: ");
+    printStack(&stack);
     return 0;
 }
